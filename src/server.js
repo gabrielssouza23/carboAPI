3f6e224a1398df46d4d4b1eadcbf049854c15d2a
Gabriel Souza <gabriel.desouzasilva2014@gmail.com>
2024-09-20 01:14:41 -0300

    render settings

--- src/server.js
+++ src/server.js
@@ -18,9 +18,10 @@ for (const { prefix, route } of getAllRoutes()) {
 
 export default async function bootstrap() {
   try {
-    await fastify.listen({ port: 3333});
+    const port = process.env.PORT || 3333; // Usa a porta atribuída pelo Render ou a 3333 como fallback
+    await fastify.listen({ port, host: '0.0.0.0' }); // Define o host como '0.0.0.0'
   } catch (err) {
     fastify.log.error(err);
-    process.exit(1);
+    process.exit(1); // Encerra o processo em caso de erro
   }
 }
